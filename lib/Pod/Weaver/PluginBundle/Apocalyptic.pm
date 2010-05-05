d26f9be292e6009a822aa7255505e2f6e8b2ddd6
Apocalypse <perl@0ne.us>
2010-05-04 19:17:28 -0700

    more tweaks to the config

---
+++ lib/Pod/Weaver/PluginBundle/Apocalyptic.pm
@@ -0,0 +1,120 @@
+package Pod::Weaver::PluginBundle::Apocalyptic;
+
+# ABSTRACT: a bundle for the most commonly-needed prep work for a pod document
+
+use namespace::autoclean;
+
+use Pod::Weaver::Config::Assembler;
+sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+
+sub mvp_bundle_config {
+  return (
+    [ '@Default/CorePrep',  _exp('@CorePrep'), {} ],
+    [ '@Default/Name',      _exp('Name'),      {} ],
+    [ '@Default/Version',   _exp('Version'),   {} ],
+
+    [ '@Default/prelude',   _exp('Region'),    { region_name => 'prelude'  } ],
+    [ 'SYNOPSIS',           _exp('Generic'),   {} ],
+    [ 'DESCRIPTION',        _exp('Generic'),   {} ],
+    [ 'OVERVIEW',           _exp('Generic'),   {} ],
+
+    [ 'ATTRIBUTES',         _exp('Collect'),   { command => 'attr'   } ],
+    [ 'METHODS',            _exp('Collect'),   { command => 'method' } ],
+    [ 'FUNCTIONS',          _exp('Collect'),   { command => 'func'   } ],
+
+    [ '@Default/Leftovers', _exp('Leftovers'), {} ],
+
+    [ '@Default/postlude',  _exp('Region'),    { region_name => 'postlude' } ],
+
+    [ '@Default/Authors',   _exp('Authors'),   {} ],
+    [ '@Default/Legal',     _exp('Legal'),     {} ],
+  )
+}
+
+1;
+
+=pod
+
+=head1 DESCRIPTION
+
+This is the bundle used by default (specifically by Pod::Weaver's
+C<new_with_default_config> method).  It may change over time, but should remain
+fairly conservative and straightforward.
+
+It is nearly equivalent to the following:
+
+  [@CorePrep]
+
+  [Name]
+  [Version]
+
+  [Region  / prelude]
+
+  [Generic / SYNOPSIS]
+  [Generic / DESCRIPTION]
+  [Generic / OVERVIEW]
+
+  [Collect / ATTRIBUTES]
+  command = attr
+
+  [Collect / METHODS]
+  command = method
+
+  [Collect / FUNCTIONS]
+  command = func
+
+  [Leftovers]
+
+  [Region  / postlude]
+
+  [Authors]
+  [Legal]
+
+=head1 TODO
+
+What I want to do is...
+
+=head2 Add my usual SUPPORT section automatically
+
+This only exists in the main module, not submodules
+
+also, automatically add the =for pod_spelling FOO BAR BAZ stuff preceding it
+
+=head2 Add DISCLAIMER OF WARRANTY head1 section
+
+DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENSE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+( Where can I get this text? Software::License? )
+
+Also, this exists only in the main module
+
+=head2 SEE ALSO section
+
+Automatically add a link from submodules to main module + whatever was specified if the section existed
+
+=head2 Add "ACKNOWLEDGEMENTS" as sub-section of AUTHOR
+
+So I can give the proper props :)
+
+=cut
